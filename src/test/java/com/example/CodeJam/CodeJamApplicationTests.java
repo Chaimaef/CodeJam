294c67852be025ba712637655db8457918bc9efd
chaimaef <chaimaefahmi@outlook.com>
2019-11-13 19:03:30 -0500

    Orignal project setup

---
+++ src/test/java/com/example/CodeJam/CodeJamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeJam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeJamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
