6cce8ecba3d2630278a874b912ff9d65db246032
AnKhangC <an.k.chau@mail.mcgill.ca>
2019-11-16 20:56:05 -0500

    before stuff

--- src/main/java/com/example/CodeJam/CodeJamApplication.java
+++ src/main/java/com/example/CodeJam/CodeJamApplication.java
@@ -3,7 +3,6 @@ package com.example.CodeJam;
 import com.example.CodeJam.DiagnosisApi.SymptomsList;
 import com.example.CodeJam.FindDoctorApi.FindDoctor;
 import com.example.CodeJam.models.diagnosis.Symptom;
-import com.example.CodeJam.models.findDoctor.Doctor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,28 +10,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.print.Doc;
 import java.util.List;
 
 @SpringBootApplication
 public class CodeJamApplication {
-	
-//	  static final String JDBC_DRIVER = "org.h2.Driver";
-//	   static final String DB_URL = "jdbc:h2:file:/Users/chaimaefahmi/Downloads/CodeJam/src/main/resources/data.sql";
-//
-//	   //  Database credentials
-//	   static final String USER = "sa";
-//	   static final String PASS = "";
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeJamApplication.class, args);
-
-		//List<Symptom> symptoms = SymptomsList.getSymptomsList();
-		FindDoctor fd = new FindDoctor();
-//		List<Doctor> doctors = fd.getDoctors("pediatrician", "ca-berkeley");
-//		System.out.println(doctors.get(0).getClinicName());
-//		System.out.println(doctors.get(0).getPhone());
-//		System.out.println(doctors.get(0).getStreet());
+		SymptomsList.getSymptomsList();
 	}
 	
 	@Bean
--- src/main/java/com/example/CodeJam/DiagnosisApi/SymptomsList.java
+++ src/main/java/com/example/CodeJam/DiagnosisApi/SymptomsList.java
@@ -1,5 +1,6 @@
 package com.example.CodeJam.DiagnosisApi;
 
+import com.example.CodeJam.Utilities.Utils;
 import com.example.CodeJam.models.diagnosis.Symptom;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -13,7 +14,7 @@ public class SymptomsList {
 
     private SymptomsList() {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFua2hhbmdjaGF1OThAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI2MDcxIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDE5LTExLTE2IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE1NzM5MzI1MTQsIm5iZiI6MTU3MzkyNTMxNH0.f2_V_Js96J3LyU6BzkkuhT4p2EO3CCdGwZfXrJt1Who&format=json&language=en-gb";
+        String url = "https://sandbox-healthservice.priaid.ch/symptoms?token=" + Utils.diagnosisKey + "&format=json&language=en-gb";
         ResponseEntity<List<Symptom>> response =
                 restTemplate.exchange(url,
                         HttpMethod.GET, null, new ParameterizedTypeReference<List<Symptom>>() {
